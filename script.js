506c7431ff2577240d8a027e708a1afd5ba5163d
David C <d.chia@proton.me>
2022-07-14 01:55:32 +0200

    Add function for one round

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function for computer's random selection:
 //selection with rock, paper, scissors is defined
 //random number from 0-3 is generated
 //matching choice from selection is picked and returned  
@@ -11,3 +12,52 @@ function computerPlay() {
 
 console.log(computerPlay());
 
+
+//function for one round player vs computer
+//compare player input with computer choice
+//if it's the same: tie
+//if player picked rock and computer picked scissor: player win
+//if player picked rock and computer picked paper: computer win
+//... 
+//return result as call for function
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return tie();
+        }
+        else if (computerSelection == "scissors") {
+            return playerWin();
+        }
+        else {
+            return computerWin();
+        }
+    }
+    else if (playerSelection == "paper") {
+        if (computerSelection == "paper") {
+            return tie();
+        }
+        else if (computerSelection == "rock") {
+            return playerWin();
+        }
+        else {
+            return computerWin();
+        }
+    }
+    else if (playerSelection == "scissors") {
+        if (computerSelection == "scissors") {
+            return tie();
+        }
+        else if (computerSelection == "paper") {
+            return playerWin();
+        }
+        else {
+            return computerWin();
+        }
+    }
+}
+
+
+//functions for tie, playerWin and computerWin
+//show return message
+//add to counters
